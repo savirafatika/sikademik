78289bd989e8c5a064b0d0852e8d1920aba47fac
Savira <savira.fatika.sf@gmail.com>
2023-04-02 13:52:07 +0700

    ubah tampilan tabel daftar pengguna menjadi semua level pengguna

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class User_model extends CI_Model
@@ -7,7 +7,7 @@ class User_model extends CI_Model
     {
         $this->db->select('u.id,u.name,u.email,u.role_id,u.is_active,u.date_created,ur.role');
         // $this->db->where('u.email !=', $email);
-        $this->db->where_in('u.role_id', [2,3]);
+        // $this->db->where_in('u.role_id', [2,3]);
         $this->db->from('user as u');
         $this->db->join('user_role as ur', 'ur.id = u.role_id');
         return $this->db->get();
